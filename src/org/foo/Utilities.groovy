d6757273f67a60f511faf3579c12efacb6cc1d38
Max Mazur <sirmax123@gmail.com>
2017-01-23 13:02:06 +0200

    test lib, no commit details, tests only

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -1,5 +1,10 @@
 package org.foo
 
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+import groovy.io.FileType
+
+
 class Utilities {
   def method1(arg) {
     println(arg)
@@ -10,39 +15,32 @@ class Utilities {
 //  }
 
 
-def buildName() {
+  def buildName() {
 
-import java.util.regex.Matcher
-import java.util.regex.Pattern
-import groovy.io.FileType
-
-
-try {
-    upstreamBuildName =  currentBuild.rawBuild.getCause(hudson.model.Cause$UpstreamCause).properties.upstreamProject
-    upstreamBuildNumber =  currentBuild.rawBuild.getCause(hudson.model.Cause$UpstreamCause).properties.upstreamBuild
-    println(upstreamBuildName)
-    println(upstreamBuildNumber)
-        
-    currentBuild.displayName = "${env.BUILD_NUMBER}-UpstreamJob=${upstreamBuildName}-UpstreamBuildNumber=${upstreamBuildNumber}"
-    currentBuild.description = currentBuild.displayName
-}
-catch (java.lang.NullPointerException e) {
-    println("No upstream job found")    
-}
-
-
-
-try {
-    specificCause = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause).properties
-    println(specificCause.userId)
-    currentBuild.displayName = "${env.BUILD_NUMBER}-startedBy: ${specificCause.userId}"
-    currentBuild.description = "${env.BUILD_NUMBER}-startedBy: ${specificCause.userId}"
-} 
-catch (java.lang.NullPointerException e) {
-    println("No user found")
-}
-
-
-}
+//    try {
+//      upstreamBuildName =  currentBuild.rawBuild.getCause(hudson.model.Cause$UpstreamCause).properties.upstreamProject
+//      upstreamBuildNumber =  currentBuild.rawBuild.getCause(hudson.model.Cause$UpstreamCause).properties.upstreamBuild
+//      println(upstreamBuildName)
+//      println(upstreamBuildNumber)
+//        
+//      currentBuild.displayName = "${env.BUILD_NUMBER}-UpstreamJob=${upstreamBuildName}-UpstreamBuildNumber=${upstreamBuildNumber}"
+//      currentBuild.description = currentBuild.displayName
+//    }
+//    catch (java.lang.NullPointerException e) {
+//      println("No upstream job found")    
+//    }
+//
+//
+//
+//    try {
+////      specificCause = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause).properties
+//      println(specificCause.userId)
+//      currentBuild.displayName = "${env.BUILD_NUMBER}-startedBy: ${specificCause.userId}"
+//      currentBuild.description = "${env.BUILD_NUMBER}-startedBy: ${specificCause.userId}"
+//    }     
+//    catch (java.lang.NullPointerException e) {
+//      println("No user found")
+//    }
+  }
 
 }
