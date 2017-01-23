3e1ff4fcc944f17895421e728f875bcec2afece1
Max Mazur <sirmax123@gmail.com>
2017-01-23 12:46:11 +0200

    fix misprinting 3

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -1,10 +1,11 @@
 package org.foo
 
 class Utilities {
-  def method1 {
-    println("method1")
-  }
-  def method2(args) {
-    println(args)
-  }
+
+//  def method1 {
+//    println("method1")
+//  }
+//  def method2(args) {
+//    println(args)
+//  }
 }
